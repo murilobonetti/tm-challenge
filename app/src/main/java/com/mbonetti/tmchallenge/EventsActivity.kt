7d962ec3c05d4c8360ebe5bee8c9b9cb352e5d71
murilobonetti <mbonetti.dev@gmail.com>
2024-02-27 16:56:45 -0300

    Add viewBinding to EventsActivity

--- app/src/main/java/com/mbonetti/tmchallenge/EventsActivity.kt
+++ app/src/main/java/com/mbonetti/tmchallenge/EventsActivity.kt
@@ -2,10 +2,16 @@ package com.mbonetti.tmchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mbonetti.tmchallenge.databinding.ActivityEventsBinding
 
 class EventsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEventsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_events)
+        binding = ActivityEventsBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
