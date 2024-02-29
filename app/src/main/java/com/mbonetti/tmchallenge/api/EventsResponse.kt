28dd3e0bed8240c6ac87f5794ca2735b8a62114a
murilobonetti <mbonetti.dev@gmail.com>
2024-02-29 15:04:40 -0300

    Add resource strings

--- app/src/main/java/com/mbonetti/tmchallenge/api/EventsResponse.kt
+++ app/src/main/java/com/mbonetti/tmchallenge/api/EventsResponse.kt
@@ -6,6 +6,6 @@ import com.squareup.moshi.Json
 
 data class EventsResponse(
     @Json(name = "_embedded")
-    val embedded: Embedded,
-    val page: Page,
+    val embedded: Embedded?,
+    val page: Page?,
 )
