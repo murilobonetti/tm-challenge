6c87d8f6842d1519d0ec4d54cc4d460ffa430287
murilobonetti <mbonetti.dev@gmail.com>
2024-03-21 10:41:08 -0300

    Remove unused data classes

--- app/src/main/java/com/mbonetti/tmchallenge/db/models/Public.kt
+++
@@ -1,8 +0,0 @@
-package com.mbonetti.tmchallenge.db.models
-
-data class Public(
-    val endDateTime: String,
-    val startDateTime: String,
-    val startTBA: Boolean,
-    val startTBD: Boolean
-)
